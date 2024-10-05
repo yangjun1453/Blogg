0f838727b6a89d18c1e9d208ce31f9f951e44eef
YangChengxxyy <853364106@qq.com>
2024-10-05 14:09:38 +0800

    第一次提交

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),VueSetupExtend() ],
-  publicDir: 'https://yangjun1453.github.io/Blogg/',
+  // publicDir: 'https://yangjun1453.github.io/Blogg/',
+  base: 'https://yangjun1453.github.io/Blogg/',
 })
